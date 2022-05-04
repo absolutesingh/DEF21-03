12dd2a27cf2d94c54f55390b4da8c3fd3a561f97
Sehajpreet Singh <sehajpreet.singh023@gmail.com>
2022-05-04 19:17:36 +0530

    Linked Lists - 3

--- Java Workspace/ChitkaraDSA/src/linkedList/MyLL.java
+++ Java Workspace/ChitkaraDSA/src/linkedList/MyLL.java
@@ -129,6 +129,96 @@ public class MyLL {
 		return prev;
 	}
 
+//	https://practice.geeksforgeeks.org/problems/intersection-point-in-y-shapped-linked-lists/1
+	static int intersectPoint(Node head1, Node head2) {
+		Node temp1 = head1;
+		Node temp2 = head2;
+
+		while (temp1 != null) {
+			while (temp2 != null) {
+				if (temp1 == temp2) {
+					return temp1.data;
+				}
+				temp2 = temp2.next;
+			}
+			temp2 = head2;
+			temp1 = temp1.next;
+		}
+		return -1;
+	}
+
+//	https://practice.geeksforgeeks.org/problems/intersection-point-in-y-shapped-linked-lists/1
+	static int intersectPointOptimized(Node head1, Node head2) {
+		Node temp1 = head1;
+		Node temp2 = head2;
+
+		int len1 = 0;
+		int len2 = 0;
+		int diff = 0;
+
+		while (temp1 != null) {
+			temp1 = temp1.next;
+			len1++;
+		}
+
+		while (temp2 != null) {
+			temp2 = temp2.next;
+			len2++;
+		}
+		// Calculated length of both lists
+		if (len1 >= len2) {
+			temp1 = head1; // Call the longer as list1
+			temp2 = head2; // Call the shorter as list2
+			diff = len1 - len2;
+		} else {
+			temp1 = head2;// Call the longer as list1
+			temp2 = head1;// Call the shorted as list2
+			diff = len2 - len1;
+		}
+
+		// Move the longer one ahead
+		for (int i = 0; i < diff; i++) {
+			temp1 = temp1.next;
+		}
+
+		// Move together and compare
+		while (temp1 != null && temp2 != null) {
+			if (temp1 == temp2)
+				return temp1.data;
+			temp1 = temp1.next;
+			temp2 = temp2.next;
+		}
+
+		// Reaching here means intersection point not found.
+		return -1;
+	}
+
+	//Only for reference -> Approach - 2
+	static void removeLoop(Node listNode, Node loopNode) {
+		Node temp = loopNode;
+		int size = 1;
+		while (loopNode.next != temp)// finding size of loop
+		{
+			loopNode = loopNode.next;
+			size++;
+		}
+
+		Node slow = listNode; // listNode is head
+		Node fast = listNode;
+
+		for (int i = 0; i < size; i++) {
+			fast = fast.next;
+		}
+
+		// Move them together
+		while (slow.next != fast.next) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+		fast.next = null;
+
+	}
+
 	public static void main(String[] args) {
 		Node head = new Node(5);
 //		System.out.println(head.data);
